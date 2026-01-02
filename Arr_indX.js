b2417a04072ff7401f00aa7ce88ba54217152a40
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-02 18:16:16 +0530

    Add files via upload

---
+++ Arr_indX.js
@@ -0,0 +1,16 @@
+function main()
+{
+    const arr=[10,20,30,40];
+
+    console.log(arr[0]);
+    console.log(arr[1]);
+    console.log(arr[2]);
+    console.log(arr[3]);
+
+
+    arr[0]= 11;
+
+    console.log(arr[0]);
+}
+
+main();
