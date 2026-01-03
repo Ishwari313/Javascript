13eb1fe3be5900581b3a1be4e90e01dfa64f6dd7
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-03 23:04:48 +0530

    Add files via upload

---
+++ div_mod.js
@@ -0,0 +1,11 @@
+function main(){
+    let ivalue =721;
+
+    console.log(ivalue); //721
+    console.log(typeof(ivalue)); //number
+    console.log(ivalue%10); //1
+
+    console.log(ivalue/10); //72
+
+}
+main(); 
